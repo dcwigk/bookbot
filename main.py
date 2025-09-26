5d56bdc915da6422c5988c9c925a99b9bb00b909
dcwigk <dev@cwigk.de>
2025-09-26 14:47:16 +0200

    split functions in simpler tasks, refactored main.py and add report

--- main.py
+++ main.py
@@ -3,13 +3,34 @@ from stats import get_num_words, count_characters, sort_characters
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         return f.read()
-    
+
+def get_sort_character_count(book: str) -> list[dict]:
+    return sort_characters(count_characters(book))
+
+def create_character_string(book: str) -> str:
+    sort_char = get_sort_character_count(book)
+    i = 0
+    char_list = []
+    while i < len(sort_char):
+        if sort_char[i]["char"].isalpha() == False:
+            i += 1
+        else:
+            char_list.append(f"{sort_char[i]["char"]}: {sort_char[i]["num"]}")
+            i += 1
+    return "\n".join(char_list)
+
+def create_report(book: str) -> None:
+    print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {get_num_words(book)} total words
+--------- Character Count -------
+{create_character_string(book)}
+============= END ===============""")
+
 def main() -> None:
     book = get_book_text("./books/frankenstein.txt")
-    num_words = get_num_words(book)
-    num_char = count_characters(book)
-    print(f"Found {num_words} total words")
-    print(sort_characters(num_char))
+    create_report(book)
 
 if __name__ == "__main__":
     main()
