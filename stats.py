51c36c0444bc140c0ccdc2afb65846ad1b5049f1
dcwigk <dev@cwigk.de>
2025-09-26 15:50:42 +0200

    refactored stats.py

--- stats.py
+++ stats.py
@@ -3,19 +3,13 @@ def get_num_words(file_contents: str) -> int:
 
 def count_characters(file_contents: str) -> dict:
     counter = {}
-    for char in file_contents:
-        lower_char = char.lower()
-        if lower_char in counter:
-            counter[lower_char] += 1
-        else:
-            counter[lower_char] = 1
+    for char in file_contents.lower():
+        counter[char] = counter.get(char, 0) + 1
     return counter
 
 def sort_characters(count_characters: dict) -> list[dict]:
-    def sort_on(items):
-        return items["num"]
     char_list = []
     for char, num in count_characters.items():
         char_list.append({"char": char, "num": num})
-    char_list.sort(reverse=True, key=sort_on) 
+    char_list.sort(reverse=True, key=lambda item: item["num"]) 
     return char_list
