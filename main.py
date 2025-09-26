43dd289ff445f67755a0a46f3ab56126b46680a8
dcwigk <dev@cwigk.de>
2025-09-26 13:16:46 +0200

    add function that sort characters on frequency

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, count_characters, sort_characters
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
@@ -7,8 +7,9 @@ def get_book_text(filepath: str) -> str:
 def main() -> None:
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
+    num_char = count_characters(book)
     print(f"Found {num_words} total words")
-    print(count_characters(book))
+    print(sort_characters(num_char))
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -7,6 +7,15 @@ def count_characters(file_contents: str) -> dict:
         lower_char = char.lower()
         if lower_char in counter:
             counter[lower_char] += 1
-            continue
-        counter[lower_char] = 1
+        else:
+            counter[lower_char] = 1
     return counter
+
+def sort_characters(count_characters: dict) -> list[dict]:
+    def sort_on(items):
+        return items["num"]
+    char_list = []
+    for char, num in count_characters.items():
+        char_list.append({"char": char, "num": num})
+    char_list.sort(reverse=True, key=sort_on) 
+    return char_list
