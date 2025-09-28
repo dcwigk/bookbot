d84e2c41018137fa6e40a5d617447e1afd759c7b
dcwigk <dev@cwigk.de>
2025-09-28 11:11:30 +0200

    apply black formatting

--- main.py
+++ main.py
@@ -3,40 +3,51 @@
 import sys
 from stats import get_num_words, count_characters, sort_characters
 
+
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         return f.read()
 
+
 def get_sort_character_count(book_text: str) -> list[dict]:
     return sort_characters(count_characters(book_text))
 
+
 def create_character_string(book_text: str) -> str:
     sorted_chars = get_sort_character_count(book_text)
-    char_lines = [f"{item["char"]}: {item["num"]}" for item in sorted_chars if item["char"].isalpha()]
+    char_lines = [
+        f"{item["char"]}: {item["num"]}"
+        for item in sorted_chars
+        if item["char"].isalpha()
+    ]
     return "\n".join(char_lines)
 
+
 def create_report(book_path: str, num_words: int, character_stats: str) -> None:
-    print(f"""============ BOOKBOT ============
+    print(
+        f"""============ BOOKBOT ============
 Analyzing book found at {book_path}...
 ----------- Word Count ----------
 Found {num_words} total words
 --------- Character Count -------
 {character_stats}
-============= END ===============""")
+============= END ==============="""
+    )
+
 
 def main() -> None:
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    
+
     book_path = sys.argv[1]
     book_text = get_book_text(book_path)
-    
+
     num_words = get_num_words(book_text)
     character_stats_string = create_character_string(book_text)
 
     create_report(book_path, num_words, character_stats_string)
 
+
 if __name__ == "__main__":
     main()
-
--- stats.py
+++ stats.py
@@ -1,15 +1,17 @@
 def get_num_words(file_contents: str) -> int:
     return len(file_contents.split())
 
+
 def count_characters(file_contents: str) -> dict:
     counter = {}
     for char in file_contents.lower():
         counter[char] = counter.get(char, 0) + 1
     return counter
 
+
 def sort_characters(character_counts: dict) -> list[dict]:
     char_list = []
     for char, num in character_counts.items():
         char_list.append({"char": char, "num": num})
-    char_list.sort(reverse=True, key=lambda item: item["num"]) 
+    char_list.sort(reverse=True, key=lambda item: item["num"])
     return char_list
